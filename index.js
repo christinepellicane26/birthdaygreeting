4339d4d35812ff93ed6a19e97fd9e04bb857067c
Christine Pellicane <51450598+christinepellicane26@users.noreply.github.com>
2020-07-27 17:17:35 -0400

    Add files via upload
    Making a mess attempting to animate CSS.

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.clear()
+// console.log("%s is %d years old", "John", 56);
+// console.log("%s is %d years old", "William", 50);
+// console.log("%s is %d years old", "Liam", 9);
+
+// for (i=0; i<5; i++);{
+//   debugger
+//   console.log(i);
+// }
+
+
+
+
+/*<script>
+{/* var x = document.getElementById("myDIV");
+
+// Start the animation with JavaScript
+function myFunction() {
+  x.style.WebkitAnimation = "mymove 4s 2"; // Code for Chrome, Safari and Opera
+  x.style.animation = "mymove 4s 2";     // Standard syntax
+}
+
+// Code for Chrome, Safari and Opera
+x.addEventListener("webkitAnimationStart", myStartFunction);
+x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+x.addEventListener("webkitAnimationEnd", myEndFunction);
+
+// Standard syntax
+x.addEventListener("animationstart", myStartFunction);
+x.addEventListener("animationiteration", myRepeatFunction);
+x.addEventListener("animationend", myEndFunction);
+
+function myStartFunction() {
+  this.innerHTML = "animationstart event occured - The animation has started";
+  this.style.backgroundColor = "pink";
+}
+
+function myRepeatFunction() {
+  this.innerHTML = "animationiteration event occured - The animation was played again";
+  this.style.backgroundColor = "lightblue";
+}
+
+function myEndFunction() {
+  this.innerHTML = "animationend event occured - The animation has completed";
+  this.style.backgroundColor = "lightgray";
+}
+</script> */
+
+// </body>
+// </html>
+
